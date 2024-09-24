271b31a242f57d5ea5aee3154f376bc0f0c89c60
aadilmufti <aadilmufti@gmail.com>
2024-09-24 15:06:02 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,6 +8,7 @@ import PortfolioContext from '../../context/context';
 const About = () => {
   const { about } = useContext(PortfolioContext);
   const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -20,17 +21,6 @@ const About = () => {
       setIsDesktop(false);
     }
   }, []);
-
-  const renderParagraphOne = () => {
-    const parts = paragraphOne.split('Y42');
-    return (
-      <p className="about-wrapper__info-text">
-        {parts[0]}
-        <a href="https://www.y42.com/" target="_blank" rel="noopener noreferrer">XXX</a>
-        {parts[1]}
-      </p>
-    );
-  };
 
   return (
     <section id="about">
@@ -47,7 +37,10 @@ const About = () => {
           <Col md={6} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
-                {renderParagraphOne()}
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
                     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
@@ -57,7 +50,7 @@ const About = () => {
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
-                    
+                    <a
                       target="_blank"
                       rel="noopener noreferrer"
                       className="cta-btn cta-btn--resume"
@@ -67,15 +60,13 @@ const About = () => {
                     </a>
                   </span>
                 )}
-                <p>
-                  
-                    href="https://open.spotify.com/album/5UJvuewgB5yqVReEFdNP7v?highlight=spotify:track:60EXhGrQgfk0OgU2drLdp0"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Check out my band, The Willows!
-                  </a>
-                </p>
+                <a
+                  href="https://open.spotify.com/album/5UJvuewgB5yqVReEFdNP7v?highlight=spotify:track:60EXhGrQgfk0OgU2drLdp0"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Check out my band, The Willows!
+                </a>
               </div>
             </Fade>
           </Col>
